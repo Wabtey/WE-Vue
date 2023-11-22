d47b9f2c9cb6829eede722a3875fb53cc2e5a104
Wabtey <wabtey@disroot.org>
2023-11-22 18:08:29 +0100

    format `TheWelcome` component

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,33 +1,30 @@
 <template>
-    <h1>hello</h1>
-    <p v-if="visible">Maintenant vous me voyez {{title}}</p>
-    <button v-on:click="foo()">ok</button>
-  </template>
-  <script lang="ts">
-  // DO NOT USE <script setup>
-  
-  //vue options API.
-  /*
+  <h1>hello</h1>
+  <p v-if="visible">Maintenant vous me voyez {{ title }}</p>
+  <button v-on:click="foo()">ok</button>
+</template>
+<script lang="ts">
+// DO NOT USE <script setup>
+
+//vue options API.
+/*
   import { defineComponent } from "vue";
   export default defineComponent({});
   */
-  
-  //class component
 
-  import { Component, Vue, toNative } from "vue-facing-decorator";
-  @Component
-  class TheWelcome extends Vue {
+//class component
 
-        title: string = 'test'
-        visible = true;
+import { Component, Vue, toNative } from 'vue-facing-decorator'
+@Component
+class TheWelcome extends Vue {
+  title: string = 'test'
+  visible = true
 
-        foo():void{
-            this.visible = !this.visible
-            this.title = 'test1'
-        }
-
+  foo(): void {
+    this.visible = !this.visible
+    this.title = 'test1'
   }
-  export default toNative(TheWelcome)
-  </script>
-  <style>
-  </style>
+}
+export default toNative(TheWelcome)
+</script>
+<style></style>
