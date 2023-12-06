94ec31b2d8b005ea8807ffea5f9130b472c246bb
Wabtey <wabtey@disroot.org>
2023-12-06 19:40:31 +0100

    add footer infos, display options

--- src/components/todos.vue
+++ src/components/todos.vue
@@ -3,15 +3,37 @@
         <h1>Tasks list</h1>
         <input v-model="input_message" placeholder="Your task">
         <button v-on:click="add_task(input_message)">add</button>
-    
-        <li v-for="todo in todos_list"
-            :key="todo.title"
-        >
-            <button v-on:click="complete_task(todo)" v-if="is_completed(todo)">x</button>
-            <button v-on:click="complete_task(todo)" v-if="!is_completed(todo)">_</button>
-            {{ todo.title }},
-            {{ todo.status }}
-        </li>
+        <br> filters: 
+        <button v-on:click="display_all_task()">All task</button>
+        <button v-on:click="display_completed_task()">Completed task</button>
+        <button v-on:click="display_in_progress_task()">InProgress task</button>
+        
+        <ul v-if="is_display_all() || is_display_in_progress()">
+            <li v-for="todo in inProgressTodos"
+                :key="todo.title"
+            >
+                <button v-on:click="complete_task(todo)" v-if="is_completed(todo)">x</button>
+                <button v-on:click="complete_task(todo)" v-if="!is_completed(todo)">_</button>
+                {{ todo.title }},
+                {{ todo.status }}
+            </li>
+        </ul>
+   
+        <ul v-if="is_display_all() || is_display_completed()">
+            <li v-for="todo in completedTodos"
+                :key="todo.title"
+            >
+                <button v-on:click="complete_task(todo)">x</button>
+                {{ todo.title }},
+                {{ todo.status }}
+            </li>
+        </ul>
+
+        <footer>
+          <p>
+            You have <span class="highlight">{{ todos_list.length }}</span> todos.
+          </p>
+        </footer>
     </div>
 
     <!-- <li v-if="!todo.isComplete">
@@ -24,9 +46,12 @@
     import { Component, Vue, toNative } from 'vue-facing-decorator'
     import TodoItem from './TODOComponent.vue'
     import TodoStatus from './todo_status'
+    import FilterMode from './filter_mode'
+    
     @Component
     class Todos extends Vue {
         input_message = 'Be happy'
+        filterMode: FilterMode = FilterMode.DisplayAll
 
         todos_list: TodoItem[] = [
             new TodoItem('Make bread', TodoStatus.InProgress, '2024-02-10'),
@@ -64,6 +89,42 @@
         is_completed(todo: TodoItem): boolean {
             return todo.status == TodoStatus.Completed;
         }
+
+        /* --------------------------------- Filters -------------------------------- */
+
+        // --- setters ---
+
+        display_all_task(): void {
+            this.filterMode = FilterMode.DisplayAll
+        }
+
+        display_completed_task(): void {
+            this.filterMode = FilterMode.DisplayCompleted
+        }
+
+        display_in_progress_task(): void {
+            this.filterMode = FilterMode.DisplayInProgress
+        }
+
+        // --- getters ---
+        
+        get inProgressTodos(): TodoItem[] {
+            return this.todos_list.filter((todo) => !this.is_completed(todo));
+        }
+
+        get completedTodos(): TodoItem[] {
+            return this.todos_list.filter(this.is_completed);
+        }
+
+        is_display_all(): boolean {
+            return this.filterMode == FilterMode.DisplayAll
+        }
+        is_display_completed(): boolean {
+            return this.filterMode == FilterMode.DisplayCompleted
+        }
+        is_display_in_progress(): boolean {
+            return this.filterMode == FilterMode.DisplayInProgress
+        }
     }
     export default toNative(Todos);
 </script>
@@ -72,5 +133,11 @@
     .todos input{
         text-align: center;
         /* justify-content: center; */
+    }
+
+    footer {
+        /* background-color: #f0f0f0; */
+        padding: 10px;
+        text-align: center;
     }
 </style>
